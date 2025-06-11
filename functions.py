ace9901294572e191d849e6386a64819fd4f4efc
Casey LaBay <cjlabay@students.everettcc.edu>
2025-06-11 22:25:58 +0000

    deleted temp files, added list

---
+++ functions.py
@@ -0,0 +1,14 @@
+# lists of acceptable inputs
+
+people = ["Bob", "Branson", "Rex", "Ronald", "Marques", "Zayden", "Elsa", "Jada", "Dani", "Julia", "Kelsie", "Anabelle"]
+nouns = ["dolphin", "crowbar", "bed", "can of peas", "soccer ball", "coffee pot", "toilet", "purse", "baseball hat", "hammer", "bottle cap", "steak knife"]
+verbs = ["prepare", "boast", "stand", "explode", "taste", "carry", "teleport", "fall", "mine", "remember", "sleep", "squeeze"]
+adjectives = ["firmer", "old", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
+emotions = ["ashamed", "excited", "proud", "afraid", "furious", "mad", "interested", "happy", "remorseful", "disappointed"]
+animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "squirrel", "gnu", "fox", "goose", "bear"]
+foods = ["radish", "beans", "mangoes", "hamburgers", "soup", "steak", "popcorn", "marshmallows", "chocolates", "ice cream"]
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "gray", "white"]
+
+def get_word(word, list, category):
+    print[list]
+    word = input(f"""Input a new {category} from the list: """);
---
+++ main.py
@@ -0,0 +1,32 @@
+# initialize lists
+# random options partially provided by perchance.org random generators
+
+people = ["Bob", "Branson", "Rex", "Ronald", "Marques", "Zayden", "Elsa", "Jada", "Dani", "Julia", "Kelsie", "Anabelle"]
+nouns = ["dolphin", "crowbar", "bed", "can of peas", "soccer ball", "coffee pot", "toilet", "purse", "baseball hat", "hammer", "bottle cap", "steak knife"]
+verbs = ["prepare", "boast", "stand", "explode", "taste", "carry", "teleport", "fall", "mine", "remember", "sleep", "squeeze"]
+adjectives = ["firmer", "old", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
+emotions = ["ashamed", "excited", "proud", "afraid", "furious", "mad", "interested", "happy", "remorseful", "disappointed"]
+animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "squirrel", "gnu", "fox", "goose", "bear"]
+foods = ["radish", "beans", "mangoes", "hamburgers", "soup", "steak", "popcorn", "marshmallows", "chocolates", "ice cream"]
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "gray", "white"]
+
+# randomly pick mad libs story
+
+import random
+randmadlib = random.randint(1, 2)
+
+# prompt user to provide words from lists, using generated story
+if randmadlib = 1;
+        person1 = input()
+
+
+
+
+
+
+
+print(f'''
+        This weekend I am going camping with {person1}. I packed my lantern, sleeping bag, and 
+        {noun1}. I am so {feeling1} to {verb1} in a tent. We are going to hike, fish, and {verb2}. 
+         If I see a {color1} {animal1} while hiking, I am going to bring it home as a pet! 
+        At night we will tell {adjective1} stories and roast {food1} around the campfire!!''')

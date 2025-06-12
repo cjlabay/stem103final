f41880d3fe7b3b049f7550e22cd68973a827bbb5
Casey LaBay <cjlabay@students.everettcc.edu>
2025-06-12 02:26:37 +0000

    madlibs function with option for random

--- madlibs.py
+++ madlibs.py
@@ -3,11 +3,11 @@
 # random options partially provided by perchance.org random generators
 
 people = ["Bob", "Branson", "Rex", "Ronald", "Marques", "Zayden", "Elsa", "Jada", "Dani", "Julia", "Kelsie", "Anabelle"]
-nouns = ["dolphin", "crowbar", "bed", "can of peas", "soccer ball", "coffee pot", "toilet", "purse", "baseball hat", "hammer", "bottle cap", "steak knife"]
-verbs = ["prepare", "boast", "stand", "explode", "taste", "carry", "teleport", "fall", "mine", "remember", "sleep", "squeeze"]
-adjectives = ["firmer", "old", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
+nouns = ["guitar", "crowbar", "bed", "old tin can", "soccer ball", "coffee pot", "toilet", "purse", "baseball hat", "hammer", "bottle cap", "steak knife"]
+verbs = ["jump", "sail", "stand", "explode", "swim", "fly", "teleport", "fall", "mine", "live", "sleep", "evolve"]
+adjectives = ["glorious", "old", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
 emotions = ["ashamed", "excited", "proud", "afraid", "furious", "mad", "interested", "happy", "remorseful", "disappointed"]
-animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "squirrel", "gnu", "fox", "goose", "bear"]
+animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "dolphin", "gnu", "fox", "goose", "bear"]
 foods = ["radish", "beans", "mangoes", "hamburgers", "soup", "steak", "popcorn", "marshmallows", "chocolates", "ice cream"]
 colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "gray", "white"]
 
@@ -24,12 +24,15 @@ food1 = ""
 
 #function to get word required by madlib with function inputs of list and specified category
 
-def get_word(word, list, category):
+import random
+def get_word(list, category):
     init_words = True
     while init_words:
-        option = input(f"Mad Lib {category} selection: {list} \nInput a shown {category}: ")
-        if option in list:
-            word = option
-            return word
+        option = input(f"Mad Lib {category} selection: {list} \nInput a shown {category} or type 1 for random: ")
+        if option == "1":
+            option = random.choice(list)
+            return option
+        elif option in list:
+            return option
         else: 
             print("Invalid option. Please choose an item from the list.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import *
+from madlibs import *
 
 # randomly pick mad libs story
 
@@ -7,15 +7,16 @@ randmadlib = random.randint(1, 2)
 
 # prompt user to provide words from lists, using generated story
 # if randmadlib == 1:
-#         get_word([people], "name")
        
-get_word(person1, [people], "name")
-get_word(noun1, [nouns], "object")
-
-
-
-
-
+person1 = get_word(people, "name")
+noun1 = get_word(nouns, "object")
+verb1 = get_word(verbs, "action")
+verb2 = get_word(verbs, "action")
+feeling1 = get_word(emotions, "emotion")
+adjective1 = get_word(adjectives, "adjective")
+food1 = get_word(foods, "food")
+animal1 = get_word(animals, "animal")
+color1 = get_word(colors, "color")
 print(f'''
         This weekend I am going camping with {person1}. I packed my lantern, sleeping bag, and 
         {noun1}. I am so {feeling1} to {verb1} in a tent. We are going to hike, fish, and {verb2}. 

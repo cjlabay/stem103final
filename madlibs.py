9d47b75c69a8fe5b3f971f5de9be775e86391e2c
Casey LaBay <cjlabay@students.everettcc.edu>
2025-06-12 05:11:03 +0000

    added second story that can be generated

--- madlibs.py
+++ madlibs.py
@@ -2,25 +2,33 @@
 # initialize lists
 # random options partially provided by perchance.org random generators
 
-people = ["Bob", "Branson", "Rex", "Ronald", "Marques", "Zayden", "Elsa", "Jada", "Dani", "Julia", "Kelsie", "Anabelle"]
+names = ["Bob", "Branson", "Rex", "Ronald", "Marques", "Zayden", "Elsa", "Jada", "Dani", "Julia", "Kelsie", "Anabelle"]
 nouns = ["guitar", "crowbar", "bed", "old tin can", "soccer ball", "coffee pot", "toilet", "purse", "baseball hat", "hammer", "bottle cap", "steak knife"]
 verbs = ["jump", "sail", "stand", "explode", "swim", "fly", "teleport", "fall", "mine", "live", "sleep", "evolve"]
-adjectives = ["glorious", "old", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
+adjectives = ["glorious", "dark", "gloomy", "funny", "short", "nice", "chewy", "shallow", "spicy", "bad", "scary", "true"]
 emotions = ["ashamed", "excited", "proud", "afraid", "furious", "mad", "interested", "happy", "remorseful", "disappointed"]
-animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "dolphin", "gnu", "fox", "goose", "bear"]
+animals = ["beaver", "raccoon", "peacock", "dog", "cat", "hedgehog", "kangaroo", "dolphin", "gnu", "rat", "squirrel", "bear"]
 foods = ["radish", "beans", "mangoes", "hamburgers", "soup", "steak", "popcorn", "marshmallows", "chocolates", "ice cream"]
 colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "gray", "white"]
+numbers = ["2", "3", "5", "7", "9", "12", "20", "50", "100", "1000"]
+times = ["seconds", "minutes", "hours", "days", "weeks", "months", "years"]
 
 #initializing variables (to be changed later by user)
-person1 = ""
+name1 = ""
 noun1 = ""
+noun2 = ""
 verb1 = ""
 verb2 = ""
 adjective1 = ""
-feeling1 = ""
+adjective2 = ""
+emotion1 = ""
 animal1 = ""
+animal2 = ""
+animal3 = ""
 color1 = ""
 food1 = ""
+number1 = ""
+time1 = ""
 
 #function to get word required by madlib with function inputs of list and specified category
 
--- main.py
+++ main.py
@@ -3,22 +3,46 @@ from madlibs import *
 # randomly pick mad libs story
 
 import random
-randmadlib = random.randint(1, 2)
+randmadlib = random.randrange(2)
 
 # prompt user to provide words from lists, using generated story
-# if randmadlib == 1:
-       
-person1 = get_word(people, "name")
-noun1 = get_word(nouns, "object")
-verb1 = get_word(verbs, "action")
-verb2 = get_word(verbs, "action")
-feeling1 = get_word(emotions, "emotion")
-adjective1 = get_word(adjectives, "adjective")
-food1 = get_word(foods, "food")
-animal1 = get_word(animals, "animal")
-color1 = get_word(colors, "color")
-print(f'''
-        This weekend I am going camping with {person1}. I packed my lantern, sleeping bag, and 
-        {noun1}. I am so {feeling1} to {verb1} in a tent. We are going to hike, fish, and {verb2}. 
-         If I see a {color1} {animal1} while hiking, I am going to bring it home as a pet! 
+
+if randmadlib == 0:   
+        name1 = get_word(names, "name")
+        noun1 = get_word(nouns, "object")
+        verb1 = get_word(verbs, "action")
+        verb2 = get_word(verbs, "second action")
+        emotion1 = get_word(emotions, "emotion")
+        adjective1 = get_word(adjectives, "adjective")
+        food1 = get_word(foods, "food")
+        animal1 = get_word(animals, "animal")
+        color1 = get_word(colors, "color")
+        number1 = get_word(numbers, "number")
+        time1 = get_word(times, "unit of time")
+        print(f'''
+        This weekend I am going camping with {name1}. I packed my lantern, sleeping bag, and {noun1}.
+        I am so {emotion1} to {verb1} in a tent. We are going to hike, fish, and {verb2}. 
+        Then we will hike through the forest for {number1} {time1}.
+        If I see a {color1} {animal1} while hiking, I am going to bring it home as a pet! 
         At night we will tell {adjective1} stories and roast {food1} around the campfire!!''')
+        
+if randmadlib == 1:
+        name1 = get_word(names, "name")
+        noun1 = get_word(nouns, "object")
+        verb1 = get_word(verbs, "second action")
+        adjective1 = get_word(adjectives, "adjective")
+        adjective2 = get_word(adjectives, "second adjective")
+        food1 = get_word(foods, "food")
+        animal1 = get_word(animals, "animal")
+        animal2 = get_word(animals, "second animal")
+        animal3 = get_word(animals, "third animal")
+        color1 = get_word(colors, "color")
+        number1 = get_word(numbers, "number")
+        time1 = get_word(times, "unit of time")
+        print(f'''
+             Dear {name1}, 
+        I am writing to you from a {adjective1} castle in an enchanted forest. I found myself here
+        one day after following a {color1} {animal1}. There are magical {animal2}s and {animal3}s
+        that {verb1}. I fall asleep each night on a bed of {food1} and dream of {adjective2} {noun1}s. 
+        It feels as though I have lived here for {number1} {time1}. I hope one day you can visit, 
+        although the only way to get here now is by finding the {color1} {animal1}!''')
